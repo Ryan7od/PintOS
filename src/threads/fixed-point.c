6ba1ce257935c9c1be2dfa7b83b8234909f83570
Damian Spigut <ds3523@pixel13.doc.ic.ac.uk>
2024-10-16 23:21:00 +0100

    Implement products and quotients for fixed_point and int manipulation

--- src/threads/fixed-point.c
+++ src/threads/fixed-point.c
@@ -2,6 +2,8 @@
 #define FRACTION 14
 #define F 1 << FRACTION
 #define INT_TO_FIXED(n) n * F
+#define ROUND_TO_ZERO(x) x / F
+#define ROUND_TO_NEAREST(x) if (x >= 0) {(x + F/2)} else {(x - F/2)}
 typedef int32_t fixed_t;
 
 
@@ -27,6 +29,30 @@ fixed_t
 subtract_fp_int(fixed_t x, int n)
 {
     return (fixed_t) x - INT_TO_FIXED(n);
+}
+
+fixed_t
+product_fp_int(fixed_t x, int n)
+{
+    return (fixed_t) x * n;
+}
+
+fixed_t
+quotient_fp_int(fixed_t x, int n)
+{
+    return (fixed_t) x / n;
+}
+
+fixed_t
+product_fp(fixed_t x, fixed_t y)
+{
+    return (fixed_t) (((int64_t) x) * y)/ F;
+}
+
+fixed_t
+quotient_fp(fixed_t x, fixed_t y)
+{
+    return (fixed_t) (((int64_t) x) * F)/ y;
 }
 
 
