0fde7a26dfdcc458b5a5c2bacef5d433cb63a3e7
ryanod <ryan7od@hotmail.com>
2024-10-20 11:58:44 +0100

    Sema waiting list sorted before upping

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -140,6 +140,8 @@ sema_up (struct semaphore *sema)
   ASSERT (sema != NULL);
 
   old_level = intr_disable ();
+
+  list_sort(&sema->waiters, thread_priority_compare, NULL);
   if (!list_empty (&sema->waiters)) 
     thread_unblock (list_entry (list_pop_front (&sema->waiters),
                                 struct thread, elem));
