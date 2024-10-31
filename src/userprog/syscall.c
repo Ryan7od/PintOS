de914ee9f8d9b12c93dc7bc18878f4ae9fa951d2
Martin Young <mmy123@vm-shell1.doc.ic.ac.uk>
2024-10-31 14:01:40 +0000

    Feat: Implement put_user in syscall.c

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -7,6 +7,7 @@
 static void syscall_handler (struct intr_frame *);
 
 static int get_user(const uint8_t *uaddr);
+static bool put_user (uint8_t *udst, uint8_t byte) UNUSED;
 
 void
 syscall_init (void) 
@@ -31,4 +32,17 @@ get_user (const uint8_t *uaddr)
   int result;
   asm ("movl $1f, %0; movzbl %1, %0; 1:" : "=&a" (result) : "m" (*uaddr));
   return result;
+}
+
+/* Writes BYTE to user address UDST.
+UDST must be below PHYS_BASE.
+Returns true if successful, false if a segfault occurred. */
+static bool
+put_user (uint8_t *udst, uint8_t byte)
+{
+  int error_code;
+  asm ("movl $1f, %0; movb %b2, %1; 1:"
+      : "=&a" (error_code), "=m" (*udst) 
+      : "q" (byte));
+  return error_code != -1;
 }
