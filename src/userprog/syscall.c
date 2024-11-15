0da2aea7484afc9e7a902614a694ff47f882194b
Martin Young <mmy123@vm-shell1.doc.ic.ac.uk>
2024-11-15 14:04:22 +0000

    Feat: Implement syscall_handler, exit system call, and args reader for syscall handling

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,11 +3,17 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 
 static void syscall_handler (struct intr_frame *);
 
 static int get_user(const uint8_t *uaddr);
 static bool put_user (uint8_t *udst, uint8_t byte) UNUSED;
+
+static void get_args (struct intr_frame *f, int *args, int num_args);
+
+/* System call functions */
+static void sys_exit(int status);
 
 void
 syscall_init (void) 
@@ -18,8 +24,51 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
+  int syscall_number;
+  int args[3];
+  if (!is_user_vaddr(f->esp))
+    sys_exit(-1); // terminate if process is invalid
+  
+  syscall_number = *(int *)f->esp;
+
+  switch (syscall_number)
+  {
+    case SYS_EXIT:
+      get_args(f, &args[0], 1);
+      sys_exit(args[0]);
+      break;
+
+    // case 
+
+    default:
+      printf("Unknown system call: %d\n", syscall_number);
+      sys_exit(-1);
+      break;
+  }
+
   thread_exit ();
+}
+
+static void 
+get_args (struct intr_frame *f, int *args, int num_args)
+{
+  int *ptr;
+  for (int i = 0; i < num_args; i++)
+  {
+    ptr = (int *)f->esp + i + 1; // +1 to skip syscall_number
+    if (!is_user_vaddr(ptr))
+      sys_exit(-1);
+    args[i] = *ptr;
+  }
+}
+
+static void
+sys_exit (int status)
+{
+  struct thread *cur = thread_current();
+  cur->exit_status = status;
+  printf("%s: exit(%d)\n", cur->name, status);
+  thread_exit();
 }
 
 /* Reads a byte at user virtual address UADDR.
