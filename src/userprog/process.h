592d5b682222ef58a35f81216367ae02e49dfbfb
ryanod <ryan7od@hotmail.com>
2024-11-19 21:32:20 +0000

    feat: Add structs for child processes

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -8,6 +8,14 @@ int process_wait (tid_t);
 void process_exit (void);
 void process_activate (void);
 
+struct child_process {
+  tid_t tid;                      // Thread ID of the child
+  struct thread *parent;          // Pointer to the parent thread
+  bool parent_alive;              // Flag indicating if the parent is alive
+  struct list_elem elem;          // List element to add to parent's child list
+  // Add other fields as necessary (e.g., exit status)
+};
+
 /* Process ID type */
 typedef int pid_t;
 
